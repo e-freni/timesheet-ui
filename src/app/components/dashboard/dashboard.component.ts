40ae1cd73c5e47b41f76cb95a85f82f9473eb5fe
Enrico Freni <enrico.freni@jaewa.com>
2022-09-06 13:51:07 +0200

    fixed days find for workday association

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -7,7 +7,7 @@ import {AccountService} from "app/services/account.service";
 import {WorkdayService} from "app/services/workday.service";
 import {Workday} from "app/models/workday.model";
 import {EditWorkdayComponent} from "app/components/dashboard/edit-workday/edit-workday.component";
-import { getTodaysDate } from 'app/utils/date-utilities';
+import {getTodaysDate} from 'app/utils/date-utilities';
 
 @Component({
   selector: 'app-dashboard',
@@ -28,7 +28,6 @@ export class DashboardComponent implements OnInit {
   ) {
   }
 
-  //FIXME bug su colorazione giorni se i numeri finali coincidono con gli outer del mese precedente
   get month(): string {
     return new Intl.DateTimeFormat("it-IT", {month: "long"}).format(this.date).toUpperCase();
   }
@@ -73,7 +72,8 @@ export class DashboardComponent implements OnInit {
   private fetchWorkdays() {
     this.workdayService.findWorkdaysByUsername(this.account.username, this.monthBeginningDate(this.date.getMonth()), this.monthEndingDate(this.date.getMonth() + 1)).subscribe(workdays => {
         this.workdays = workdays;
-        this.workdays.forEach(wd => this.monthDays.find(d => d.monthDayNumber == new Date(wd.date).getDate()).workday = wd);
+        const onlyWorkDays = this.monthDays.filter(d => !d.outerMonths)
+        this.workdays.forEach(wd => onlyWorkDays.find(d => d.monthDayNumber == new Date(wd.date).getDate()).workday = wd);
       }
     )
   }
@@ -114,7 +114,7 @@ export class DashboardComponent implements OnInit {
   }
 
   openDay(day: Day) {
-    if(day.outerMonths) {
+    if (day.outerMonths) {
       return
     }
     this.matDialog.open(EditWorkdayComponent, {
@@ -123,10 +123,12 @@ export class DashboardComponent implements OnInit {
       hasBackdrop: true,
       enterAnimationDuration: "100ms",
       exitAnimationDuration: "100ms",
-      data: {day: day, calendarDate: new Date(this.date.getFullYear(), this.date.getMonth(), day.monthDayNumber, 0, 0, 0)}
-    }).afterClosed().subscribe((workDayHasBeenChanged) =>{
-      console.log(workDayHasBeenChanged);
-      if(workDayHasBeenChanged){
+      data: {
+        day: day,
+        calendarDate: new Date(this.date.getFullYear(), this.date.getMonth(), day.monthDayNumber, 0, 0, 0)
+      }
+    }).afterClosed().subscribe((workDayHasBeenChanged) => {
+      if (workDayHasBeenChanged) {
         this.fetchWorkdays()
       }
     })
