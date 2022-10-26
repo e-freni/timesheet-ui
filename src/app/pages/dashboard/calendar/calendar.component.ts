dfa158512c2d0cf0f3d5755db3083f6f80001922
Enrico Freni <enrico@freni.it>
2022-10-26 21:26:31 +0200

    added special days support

--- src/app/pages/dashboard/calendar/calendar.component.ts
+++ src/app/pages/dashboard/calendar/calendar.component.ts
@@ -1,46 +1,77 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
-import {MatDialog} from '@angular/material/dialog';
-import {WeekDay} from "@angular/common";
-import {Day} from "app/models/day";
-import {Account} from "app/models/account.model";
-import {AccountService} from "app/services/account.service";
-import {WorkdayService} from "app/services/rest/workday.service";
-import {Workday} from "app/models/workday.model";
-import {EditWorkdayComponent} from "app/pages/dashboard/calendar/edit-workday/edit-workday.component";
-import {getMonth, getYear} from 'app/utils/date-utilities';
-import {DateService} from "app/services/date.service";
-import {Subscription} from "rxjs";
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { WeekDay } from '@angular/common';
+import { Day } from 'app/models/day';
+import { Account } from 'app/models/account.model';
+import { AccountService } from 'app/services/account.service';
+import { WorkdayService } from 'app/services/rest/workday.service';
+import { Workday } from 'app/models/workday.model';
+import { EditWorkdayComponent } from 'app/pages/dashboard/calendar/edit-workday/edit-workday.component';
+import { getMonth, getYear } from 'app/utils/date-utilities';
+import { DateService } from 'app/services/date.service';
+import { Subscription } from 'rxjs';
+import {
+  ALL_SAINTS_DAY,
+  APRIL_TWENTY_FIFTH,
+  BOXING_DAY,
+  calculateEaster,
+  calculateEasterMonday,
+  CHRISTMAS,
+  EPIPHANY,
+  IMMACULATE_CONCEPTION,
+  MID_AUGUST,
+  MIDSUMMER,
+  NEW_YEARS_EVE,
+  REPUBLIC_DAY,
+  SpecialDay,
+  WORKERS_DAY,
+} from 'app/models/special-days';
 
 @Component({
   selector: 'app-calendar',
   templateUrl: './calendar.component.html',
-  styleUrls: ['./calendar.component.css']
+  styleUrls: ['./calendar.component.css'],
 })
 export class CalendarComponent implements OnInit, OnDestroy {
   date: Date;
   account: Account = null;
 
-  monthDays: Day[] = []
+  monthDays: Day[] = [];
   workdays: Workday[] = [];
 
   private workdaySubscription: Subscription;
   private dateSubscription: Subscription;
   private accountSubscription: Subscription;
+  private readonly standardNonWorkingDaysNames: string[] = ['Domenica', 'Sabato'];
+  private specialDays: SpecialDay[] = [
+    NEW_YEARS_EVE,
+    APRIL_TWENTY_FIFTH,
+    calculateEaster(new Date().getFullYear()),
+    calculateEasterMonday(new Date().getFullYear()),
+    WORKERS_DAY,
+    EPIPHANY,
+    MIDSUMMER,
+    REPUBLIC_DAY,
+    MID_AUGUST,
+    ALL_SAINTS_DAY,
+    IMMACULATE_CONCEPTION,
+    CHRISTMAS,
+    BOXING_DAY,
+  ];
 
   constructor(
     private matDialog: MatDialog,
     private accountService: AccountService,
     private dateService: DateService,
-    private workdayService: WorkdayService,
-  ) {
-  }
+    private workdayService: WorkdayService
+  ) {}
 
   get month(): string {
-    return getMonth(this.date)
+    return getMonth(this.date);
   }
 
   get year(): string {
-    return getYear(this.date)
+    return getYear(this.date);
   }
 
   ngOnInit(): void {
@@ -52,9 +83,9 @@ export class CalendarComponent implements OnInit, OnDestroy {
         next: (date: Date | null) => {
           this.account = account;
           this.date = date;
-          this.createMonthCalendar()
-          this.fetchWorkdays()
-        }
+          this.createMonthCalendar();
+          this.fetchWorkdays();
+        },
       });
     });
   }
@@ -69,26 +100,76 @@ export class CalendarComponent implements OnInit, OnDestroy {
     const date = new Date(this.date.getFullYear(), this.date.getMonth(), 1);
 
     while (date.getMonth() === this.date.getMonth()) {
-      const day = new Day(date.getDate(), date.getDay(), false)
-      this.monthDays.push(day)
+      const day = new Day(date.getDate(), date.getDay(), false);
+      this.monthDays.push(day);
       date.setDate(date.getDate() + 1);
     }
-    this.addOtherMonthsDays()
+    this.addOtherMonthsDays();
+  }
+
+  isNotWorkingDay(day: Day): boolean {
+    return this.standardNonWorkingDaysNames.includes(day.dayName);
+  }
+
+  isSpecialDay(day: Day): boolean {
+    const dateInStringFormat: string = `${day.monthDayNumber}/${this.date.getMonth() + 1}`;
+    return this.specialDays.some((d: SpecialDay) => {
+      return d.holidayDate === dateInStringFormat;
+    });
+  }
+
+  getSpecialDayName(day: Day): string {
+    const dateInStringFormat: string = `${day.monthDayNumber}/${this.date.getMonth() + 1}`;
+    return this.specialDays.find((d: SpecialDay) => {
+      return d.holidayDate === dateInStringFormat;
+    }).holidayName;
+  }
+
+  openDay(day: Day) {
+    if (day.outerMonths) {
+      return;
+    }
+    this.matDialog
+      .open(EditWorkdayComponent, {
+        width: '50%',
+        disableClose: true,
+        hasBackdrop: true,
+        enterAnimationDuration: '100ms',
+        exitAnimationDuration: '100ms',
+        data: {
+          day: day,
+          calendarDate: new Date(this.date.getFullYear(), this.date.getMonth(), day.monthDayNumber, 0, 0, 0),
+        },
+      })
+      .afterClosed()
+      .subscribe(workDayHasBeenChanged => {
+        if (workDayHasBeenChanged) {
+          this.createMonthCalendar();
+          this.fetchWorkdays();
+          this.reloadConnectedComponents();
+        }
+      });
   }
 
   private fetchWorkdays() {
     if (this.account) {
-
       if (this.workdaySubscription) {
         this.workdaySubscription.unsubscribe();
       }
 
-      this.workdaySubscription = this.workdayService.findWorkdaysByUsername(this.account.username, this.monthBeginningDate(this.date.getMonth()), this.monthEndingDate(this.date.getMonth() + 1)).subscribe(workdays => {
+      this.workdaySubscription = this.workdayService
+        .findWorkdaysByUsername(
+          this.account.username,
+          this.monthBeginningDate(this.date.getMonth()),
+          this.monthEndingDate(this.date.getMonth() + 1)
+        )
+        .subscribe(workdays => {
           this.workdays = workdays;
-          const onlyWorkDays = this.monthDays.filter(d => !d.outerMonths)
-          this.workdays.forEach(wd => onlyWorkDays.find(d => d.monthDayNumber == new Date(wd.date).getDate()).workday = wd);
-        }
-      )
+          const onlyWorkDays = this.monthDays.filter(d => !d.outerMonths);
+          this.workdays.forEach(
+            wd => (onlyWorkDays.find(d => d.monthDayNumber == new Date(wd.date).getDate()).workday = wd)
+          );
+        });
     }
   }
 
@@ -100,21 +181,14 @@ export class CalendarComponent implements OnInit, OnDestroy {
     return new Date(this.date.getFullYear(), month, 0).toLocaleDateString();
   }
 
-  private readonly standardNonWorkingDaysNames = ['Domenica', 'Sabato'];
-  private readonly specialNonWorkingDaysDates = ['']; //TODO special days like christmas
-
-  isNotWorkingDay(day: Day): boolean {
-    return this.standardNonWorkingDaysNames.includes(day.dayName)
-  }
-
   private addOtherMonthsDays() {
     let now = new Date(this.date.getFullYear(), this.date.getMonth(), 1);
 
     const firstMonthDay = 0;
     if (this.monthDays[firstMonthDay].weekdayValue !== WeekDay.Monday) {
       while (now.getDay() != WeekDay.Monday) {
-        now.setDate(now.getDate() - 1)
-        this.monthDays.unshift(new Day(now.getDate(), now.getDay(), true))
+        now.setDate(now.getDate() - 1);
+        this.monthDays.unshift(new Day(now.getDate(), now.getDay(), true));
       }
     }
 
@@ -124,35 +198,11 @@ export class CalendarComponent implements OnInit, OnDestroy {
     const lastMonthDay = this.monthDays.length - 1;
     if (this.monthDays[lastMonthDay].weekdayValue !== WeekDay.Sunday) {
       while (now.getDay() != WeekDay.Sunday) {
-        now.setDate(now.getDate() + 1)
-        this.monthDays.push(new Day(now.getDate(), now.getDay(), true))
+        now.setDate(now.getDate() + 1);
+        this.monthDays.push(new Day(now.getDate(), now.getDay(), true));
       }
     }
   }
-
-  openDay(day: Day) {
-    if (day.outerMonths) {
-      return
-    }
-    this.matDialog.open(EditWorkdayComponent, {
-      width: '50%',
-      disableClose: true,
-      hasBackdrop: true,
-      enterAnimationDuration: "100ms",
-      exitAnimationDuration: "100ms",
-      data: {
-        day: day,
-        calendarDate: new Date(this.date.getFullYear(), this.date.getMonth(), day.monthDayNumber, 0, 0, 0)
-      }
-    }).afterClosed().subscribe((workDayHasBeenChanged) => {
-      if (workDayHasBeenChanged) {
-        this.createMonthCalendar()
-        this.fetchWorkdays()
-        this.reloadConnectedComponents();
-      }
-    })
-  }
-
 
   private reloadConnectedComponents() {
     this.dateService.setDate(this.date);
--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,8 +1,8 @@
-import {Component, OnInit} from '@angular/core';
-import {PageService} from "app/services/page.service";
-import {Subscription} from "rxjs";
-import {Account} from "app/models/account.model";
-import {AccountService} from "app/services/account.service";
+import { Component, OnInit } from '@angular/core';
+import { PageService } from 'app/services/page.service';
+import { Subscription } from 'rxjs';
+import { Account } from 'app/models/account.model';
+import { AccountService } from 'app/services/account.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -12,20 +12,19 @@ export class DashboardComponent implements OnInit {
   page: string;
   pageSubscription: Subscription;
   selectedTab: number;
-
-  constructor(
-    private pageService: PageService,
-    private accountService: AccountService,
-  ) {
-  }
+  CALENDAR = 0;
+  ANALYTICS = 1;
 
   //TODO alert of missing hours on the last 7 or 5 or 3 days of the month?
-  //TODO send email popover
+  //TODO send email for real
   //TODO admin add user
   //TODO user change password
   //TODO put all css in style classes and optimize it
   //TODO make a mobile view
-  //TODO handle special days (like christmas!)
+  //TODO alert when logging in special days (like christmas!)
+  //TODO DARK MODE!
+
+  constructor(private pageService: PageService, private accountService: AccountService) {}
 
   ngOnInit(): void {
     this.accountService.getObservableAccount().subscribe((account: Account | null) => {
@@ -35,13 +34,10 @@ export class DashboardComponent implements OnInit {
       this.pageSubscription = this.pageService.getCurrentPage().subscribe({
         next: page => {
           this.page = page;
-        }
+        },
       });
     });
   }
-
-  CALENDAR = 0
-  ANALYTICS = 1
 
   switchToAnalytics() {
     this.selectedTab = this.ANALYTICS;
